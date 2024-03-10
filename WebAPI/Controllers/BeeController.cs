e6c125305af66d7b61c1fc4b6e1c2e0e940880b9
Lianuch <a.nyzhnyk@nltu.lviv.ua>
2024-03-10 21:57:28 +0200

    bee class and controller, initialised migration + db context

---
+++ WebAPI/Controllers/BeeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Entities;
+
+namespace WebAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class BeeController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAllBees()
+    {
+        var bees = new List<Bee>
+        {
+            new Bee()
+            {
+                Id = 1,
+                Name = "Worker",
+                FirstName = "Steve",
+                LastName = "Stevenson",
+                Place = "New Zeland"
+                    
+            }
+        };
+        /*return bees;*/
+        return Ok(bees);
+    }
+}
---
+++ WebAPI/Entities/Bee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Entities;
+
+public class Bee
+{
+    public int Id { get; set; }
+    [Required]
+    public  string Name { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+    public  string LastName { get; set; } = string.Empty;
+    public  string Place { get; set; } = string.Empty;
+     
+     
+}
